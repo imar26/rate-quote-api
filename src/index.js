a8ca95a38e8a192996bf486d6f68bca6540abe60
imar26 <aadeshranderia26@gmail.com>
2018-09-08 13:12:08 -0400

    folder structure creatde

--- src/index.js
+++ src/index.js
@@ -8,7 +8,22 @@ import './index.css';
 // Import Containers
 import App from './containers/App';
 
+// Import Reducers
+import rootReducer from './reducers/rootReducer';
+
+// Import Mappers
+import { mapStateToProps, mapDispatchToProps } from './mappers';
+
+import { Provider, connect } from 'react-redux';
+import { createStore } from 'redux';
+
+let store = createStore(rootReducer);
+
+const Root = connect(mapStateToProps, mapDispatchToProps)(App);
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <Root/>
+    </Provider>,
     document.getElementById("root")
 );
